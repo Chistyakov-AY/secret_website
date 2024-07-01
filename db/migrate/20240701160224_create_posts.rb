732105ace3c30fe847faba973ceab3fc0b5414eb
Aleksander <chistyakov_ay@mail.ru>
2024-07-01 19:14:55 +0300

    Created posts

---
+++ db/migrate/20240701160224_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.string :body, null: false
+      t.boolean :publication, default: false
+
+      t.timestamps
+    end
+  end
+end
